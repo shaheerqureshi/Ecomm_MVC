39a8f87bf2e2c3214323f043e228e5a6ec275ff1
shaheerqureshi <shaheerqureshi334@gmail.com>
2023-11-09 06:06:08 -0500

    Read and Update Added

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -17,5 +17,18 @@ namespace BulkyWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return View();
+        }
     }
 }
