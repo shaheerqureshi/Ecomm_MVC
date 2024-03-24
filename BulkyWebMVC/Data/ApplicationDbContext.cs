c335e36897defe2636b5bc920059dbb0d985db87
shaheerqureshi <shaheerqureshi334@gmail.com>
2024-03-24 01:51:12 -0400

    MigrationAdded TableAdded

---
+++ BulkyWebMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BulkyWebMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories{ get; set; }
+
+    }
+}
