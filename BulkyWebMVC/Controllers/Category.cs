2b269cac0472bc417a6dd612390b74e7d1f75c6c
shaheerqureshi <shaheerqureshi334@gmail.com>
2024-03-24 02:03:41 -0400

    Initial Data Seeded to DB

---
+++ BulkyWebMVC/Controllers/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWebMVC.Controllers
+{
+    public class Category : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- BulkyWebMVC/Data/ApplicationDbContext.cs
+++ BulkyWebMVC/Data/ApplicationDbContext.cs
@@ -12,5 +12,17 @@ namespace BulkyWebMVC.Data
 
         public DbSet<Category> Categories{ get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Comedy", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Sci Fi", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "History", DisplayOrder = 4 }
+
+                );
+            
+        }
+
     }
 }
