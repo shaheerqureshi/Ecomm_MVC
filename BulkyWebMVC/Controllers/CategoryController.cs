a25108984a41620bb96ce5b5ebb11427b2922977
shaheerqureshi <shaheerqureshi334@gmail.com>
2024-03-24 23:45:46 -0400

    Read data from database

---
+++ BulkyWebMVC/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkyWebMVC.Data;
+using BulkyWebMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BulkyWebMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
