976f19fd7df9b7b5903cd15434b11fb55f14a3c1
shaheerqureshi <shaheerqureshi334@gmail.com>
2024-03-31 00:39:13 -0400

    CRUD actions complete

--- BulkyWebMVC/Controllers/CategoryController.cs
+++ BulkyWebMVC/Controllers/CategoryController.cs
@@ -24,9 +24,78 @@ namespace BulkyWebMVC.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Name and display order cannopt be same");
+            }
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+            
+            
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? catfromDb = _db.Categories.Find(id);
+            //Category? catfromDb1 = _db.Categories.FirstOrDefault(u=>u.Id ==id);
+            //Category? catfromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            if(catfromDb == null)
+            {
+                return NotFound();
+            }
+            return View(catfromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Name and display order cannopt be same");
+            }
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+            
+            
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? catfromDb = _db.Categories.Find(id);
+            //Category? catfromDb1 = _db.Categories.FirstOrDefault(u=>u.Id ==id);
+            //Category? catfromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            if (catfromDb == null)
+            {
+                return NotFound();
+            }
+            return View(catfromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
             _db.SaveChanges();
-            return RedirectToAction("Index", "Category");
+                return RedirectToAction("Index", "Category");
         }
     }
 }
--- BulkyWebMVC/Models/Category.cs
+++ BulkyWebMVC/Models/Category.cs
@@ -8,9 +8,11 @@ namespace BulkyWebMVC.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(100)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
-        [DisplayName("Display Name")]
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage="Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
