8b3834a1275f7b9d8395425d6104ec4f7bb701c7
shaheerqureshi <shaheerqureshi334@gmail.com>
2024-04-01 00:44:40 -0400

    beautified all pages

--- BulkyWebRazor/Pages/Categories/Index.cshtml.cs
+++ BulkyWebRazor/Pages/Categories/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using BulkyWebRazor.Data;
+using BulkyWebRazor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,17 @@ namespace BulkyWebRazor.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        public List<Category> catList { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+            
+        }
         public void OnGet()
         {
+            catList = _db.CategoriesRazorTbl.ToList();
         }
     }
 }
