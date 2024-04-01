a9535ff585a7b1c81c6ec8f8a700911818e6aa8d
shaheerqureshi <shaheerqureshi334@gmail.com>
2024-04-01 00:08:31 -0400

    Razor Page DB updated

---
+++ BulkyWebRazor/Migrations/20240401040602_addCategorytoDB.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BulkyWebRazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCategorytoDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CategoriesRazorTbl",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DisplayOrder = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoriesRazorTbl", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CategoriesRazorTbl",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Action" },
+                    { 2, 2, "Comedy" },
+                    { 3, 3, "Sci Fi" },
+                    { 4, 4, "History" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoriesRazorTbl");
+        }
+    }
+}
